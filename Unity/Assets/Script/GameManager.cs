2e10173605e106144aaffd1338c003f46391b996
Leonardo Ballari <leonardo.ballari@aveva.com>
2023-02-05 00:07:07 +0100

    GameMAnger

--- Unity/Assets/Script/GameManager.cs
+++ Unity/Assets/Script/GameManager.cs
@@ -6,17 +6,25 @@ public class GameManager : MonoBehaviour
 {
     public PlayerManager[] players;
     public CameraMovement cam;
+    private WebSocketClient wsc;
+
     void Start()
     {
+        wsc = GameObject.FindObjectOfType<WebSocketClient>();
         InvokeRepeating("AddPieceToPlayer",2,0.1f);
         InvokeRepeating("SpeedTime",5,5);
     }
 
     public void AddPieceToPlayer()
     {
+        if(wsc.isPlaying)
+        {
+           
+        }
+
         foreach(PlayerManager pm in players)
-            pm.AddPieceToRoot();
-        
+                pm.AddPieceToRoot();
+            
         cam.AddMovement();
     }
 
