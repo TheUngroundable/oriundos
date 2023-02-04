764286e5214dd0c0d81e6bc26b188f757b72c90c
Umberto Arduini <35199075+TheUngroundable@users.noreply.github.com>
2023-02-04 23:47:45 +0100

    better game flow

--- Node/index.js
+++ Node/index.js
@@ -6,27 +6,70 @@ const wss = new WebSocket.Server({ port: 8080 }, () => {
 const CLIENTS = []
 let unityClient = undefined
 
+const numberOfPlayers = 2
+let numberOfReady = 0
+
+
 wss.on('connection', function connection(ws) {
 
     const data = {
-        id: CLIENTS.length,
+        id: unityClient ? CLIENTS.length : 0,
         command: "CONNECTION"
     }
-    if(!unityClient){
-        unityClient = ws
-    }
+
     CLIENTS.push(ws);
     
     ws.send(JSON.stringify(data))
+    
+    if(!unityClient){
+        unityClient = ws
+    }
+
+    
 
     ws.on('message', (data)=>{
         const json = JSON.parse(data)
-         const message = {
-            id: json.id,
-            command: json.command,
-            direction: json.direction
-         }
-        unityClient.send(JSON.stringify(message))
+        console.log(json)
+        if(json.command === 'JOINED'){
+            if(json.type === 'UNITY'){
+                unityClient = ws
+                console.log("Unity has joined the game")
+            } else if(json.type === 'PLAYER'){
+                console.log("Player has joined the room")
+            }
+        }
+
+        if(json.command === 'MOVEMENT'){
+            console.log("Player "+json.id+" has moved to "+json.value)
+            const message = {
+                id: json.id,
+                command: json.command,
+                value: json.value
+            }
+            unityClient.send(JSON.stringify(message))
+        }
+
+        if(json.command === 'READY'){
+            if(json.value){
+                numberOfReady++;
+                console.log("Player "+json.id+" is ready")
+            } else {
+                numberOfReady--;
+                console.log("Player "+json.id+" is not ready")
+            }
+        }
+
+        
+
+        if(numberOfReady == numberOfPlayers+1){
+            const message = {
+                id: -1,
+                command: 'STARTED',
+                value: 1
+            }
+            CLIENTS.forEach(client => client.send(JSON.stringify(message)))
+            console.log("Game has started")
+        }
     })
 })
 wss.on('listening', () => {
